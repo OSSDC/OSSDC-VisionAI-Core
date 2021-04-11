6a45bee16fa297a351719682d30731612a6a829c
Marius Slavescu <marius@slavescu.net>
2021-04-11 12:08:18 -0400

    add install script for mediapipe demo

---
+++ install-ossdc-visionai-media-pipe-demo.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/OSSDC/OSSDC-VisionAI-Core
+pip install aiortc aiohttp websockets python-engineio==3.14.2 python-socketio[client]==4.6.0 youtube-dl
+pip install mediapipe
+cd OSSDC-VisionAI-Core
+#./demo-reel.sh YourRoomName
